22e702ad3964d952d4b6218a71ebbc69b50044ab
Vitoria Santos <vick_santos_vick@hotmail.com>
2023-08-14 23:27:38 -0300

    --- adicionando conversão de csv para lista de dicionario

--- application/repository/guide_repository.py
+++ application/repository/guide_repository.py
@@ -1,3 +1,4 @@
+import csv
 import json
 from supabase import create_client
 
@@ -6,13 +7,28 @@ with open("application/config.json", "r") as f:
 
 supabase = create_client(appsettings["SUPABASE_URL"],appsettings["SUPABASE_KEY"])
 
-class GuideRepository:
 
+class GuideRepository:
     def __init__(self):
         self.collection = supabase.table('guide')
+        self.guideData = self.csv_to_list("application/services/guia-de-turismo.csv")
 
     def createGuide(self, guide):
         self.collection.insert(guide).execute()
 
     def getGuides(self):
         return self.collection.select('*').execute()
+
+    def csv_to_list(self,path):
+        dicts = []
+
+        # read csv file
+        with open(path, encoding='utf-8') as csvf:
+            # load csv file data using csv library's dictionary reader
+            leitor_de_csv = csv.DictReader(csvf)
+
+            # convert each csv row into python dict
+            for row in leitor_de_csv:
+                # add this python dict in list
+                dicts.append(row)
+        return dicts
