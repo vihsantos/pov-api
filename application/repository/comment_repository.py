d632371d6f2e51bc39e3d7c40db9aef2274b89e8
vitoriasantos <vitoria.santos@salvador.ba.gov.br>
2024-01-04 12:59:17 -0300

    --- adicionando métodos no repository de comentários

--- application/repository/comment_repository.py
+++ application/repository/comment_repository.py
@@ -5,11 +5,20 @@ from supabase import create_client
 with open("application/config.json", "r") as f:
     appsettings = json.load(f)
 
-supabase = create_client(appsettings["SUPABASE_URL"],appsettings["SUPABASE_KEY"])
+supabase = create_client(appsettings["SUPABASE_URL"], appsettings["SUPABASE_KEY"])
+
 
 class CommentRepository:
     def __init__(self):
         self.collection = supabase.table('comment')
 
     def createComment(self, comment):
-        self.collection.insert(comment).execute()
+        self.collection.insert(comment).execute();
+
+    def findCommentsOfPost(self, post_id):
+        comments = self.collection.select('*').eq('post_id', post_id).execute().data
+        return comments
+
+    def findCommentsOfTrail(self, trail_id):
+        comments = self.collection.select('*').eq('trail_id', trail_id).execute().data
+        return comments
