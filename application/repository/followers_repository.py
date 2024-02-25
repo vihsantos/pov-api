cb2fdce1a95d76c0d6bac2edeebd739a3c0537d3
vihsantos <vick_santos_vick@hotmail.com>
2024-02-24 22:27:33 -0300

    --- ajustando método pra seguir

--- application/repository/followers_repository.py
+++ application/repository/followers_repository.py
@@ -15,8 +15,8 @@ class FollowersRepository:
     def follow(self, follow):
         return self.collection.insert(follow).execute().data
 
-    def unfollow(self, id):
-        return self.collection.delete().eq("id", id).execute().data
+    def unfollow(self, unfollow):
+        return self.collection.delete().eq("user_seguidor", unfollow["user_seguidor"]).eq("user_seguindo", unfollow["user_seguindo"]).execute().data
 
     def getFollowersByID(self, id):
         return self.collection.select('*').eq("user_seguindo", id).execute().data
