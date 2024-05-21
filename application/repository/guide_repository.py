d1fd9c7e514d2484548fe98814f40df8d78aeadd
vihsantos <vick_santos_vick@hotmail.com>
2024-05-21 20:44:16 -0300

    --- adicionando campos para serem retornados

--- application/repository/guide_repository.py
+++ application/repository/guide_repository.py
@@ -62,6 +62,6 @@ class GuideRepository:
     def getInfosGuide(self, user):
         guide_id = self.collection_userguide.select('guide_id').eq("user_id", user).execute().data[0]
 
-        guide = self.collection.select('cod_cadastur, data_vencimento, areaatuacao').eq("id", guide_id["guide_id"]).execute().data[0]
+        guide = self.collection.select('cod_cadastur, data_vencimento, areaatuacao, estado, municipio').eq("id", guide_id["guide_id"]).execute().data[0]
 
         return guide
